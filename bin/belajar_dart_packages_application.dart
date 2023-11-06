7c99be5fca062493e0390ee62f7e43e14b774916
lauvinfox <miftahilmanakun@gmail.com>
2023-11-06 18:41:21 +0700

    dart application initial

---
+++ bin/belajar_dart_packages_application.dart
@@ -0,0 +1,5 @@
+import 'package:belajar_dart_packages_application/belajar_dart_packages_application.dart' as belajar_dart_packages_application;
+
+void main(List<String> arguments) {
+  print('Hello world: ${belajar_dart_packages_application.calculate()}!');
+}
---
+++ test/belajar_dart_packages_application_test.dart
@@ -0,0 +1,8 @@
+import 'package:belajar_dart_packages_application/belajar_dart_packages_application.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
